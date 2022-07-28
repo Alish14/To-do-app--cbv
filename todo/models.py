5734bcea71f4bc47e454e1ba1764c3f935a16f44
Alireza moradi <alish14.mod@gmail.com>
2022-07-28 22:37:38 +0430

    add future search and change edit template

--- todo/models.py
+++ todo/models.py
@@ -8,6 +8,7 @@ class Task(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=200)
+    #description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
--- todo/urls.py
+++ todo/urls.py
@@ -4,7 +4,7 @@ from .views import (TaskList,
 app_name = 'todo'
 urlpatterns = [
     path('', TaskList.as_view(), name="list_task"),
-    path('create', CreateTask.as_view(), name='create_task'),
+    path('create/', CreateTask.as_view(), name='create_task'),
     path('complete/<int:pk>', TaskComplete.as_view(), name='complete_task'),
     path('edit/<int:pk>', TaskEdit.as_view(), name='edit_task'),
     path('delete/<int:pk>', DeleteTask.as_view(), name='delete_task'),
