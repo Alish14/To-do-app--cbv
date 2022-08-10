b60344ac347455aa221ffc41e473fd7a8e17f365
Alireza moradi <alish14.mod@gmail.com>
2022-08-10 17:11:11 +0430

    creat api folder

--- core/urls.py
+++ core/urls.py
@@ -21,23 +21,24 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('todo.urls')),
-    path("accounts/", include("accounts.urls")),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_reset/password_change_done.html'),
+     path('admin/', admin.site.urls),
+     path('', include('todo.urls')),
+     path("accounts/", include("accounts.urls")),
+     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_reset/password_change_done.html'),
          name='password_change_done'),
 
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='accounts/password_reset/password_change.html'),
+     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='accounts/password_reset/password_change.html'),
          name='password_change'),
 
-    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset/password_reset_done.html'),
+     path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset/password_reset_done.html'),
          name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(),
+     path('password_reset/', auth_views.PasswordResetView.as_view(),
          name='password_reset'),
 
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset/password_reset_complete.html'),
+     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset/password_reset_complete.html'),
          name='password_reset_complete'),
+     path('api/v1/',include('todo.api.v1.urls')),
 ]
